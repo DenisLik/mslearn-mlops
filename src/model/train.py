60ef20193d87ca12e6bfd40fb1781ee8edc34267
delick <delick2@gmail.com>
2025-01-07 15:27:09 +0400

    2: change train.py for exercise 0

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,15 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+import mlflow
 
 
 # определить функции
 def main(args):
     # TO DO: включить автологирование
-
+    mlflow.autolog()
 
     # читать данные
     df = get_csvs_df(args.training_data)
@@ -34,7 +37,17 @@ def get_csvs_df(path):
 
 
 # TO DO: добавить функцию для разделения данных
-
+def split_data(df):
+    X, y = df[['Pregnancies',
+               'PlasmaGlucose',
+               'DiastolicBloodPressure',
+               'TricepsThickness',
+               'SerumInsulin',
+               'BMI',
+               'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
