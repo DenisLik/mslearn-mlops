1e90890122423be1d8b6cb70cc4b75cc72a924bf
delick <delick2@gmail.com>
2025-01-15 12:19:22 +0400

    6: Working with Challenge 4:
    - updated 04-code-checks.yml: changed uses: ref: develop
    
    - train.py: shorting leights of several code rows to 79 characters

--- src/model/train.py
+++ src/model/train.py
@@ -29,10 +29,14 @@ def main(args):
 
 def get_csvs_df(path):
     if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+        raise RuntimeError(
+            f"Cannot use non-existent path provided: {path}"
+            )
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+        raise RuntimeError(
+            f"No CSV files found in provided data path: {path}"
+            )
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
@@ -46,13 +50,17 @@ def split_data(df):
                'BMI',
                'DiabetesPedigree',
                'Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.30,
+                                                        random_state=0)
     return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # обучаем модель
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train,
+                                                             y_train)
 
 
 def parse_args():
